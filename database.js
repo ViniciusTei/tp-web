abf6baefa221197ae6787647d6b5383654f671bb
ViniciusTei <viniciustprates@gmail.com>
2023-06-24 18:10:43 -0300

    fix mysql config options

--- database.js
+++ database.js
@@ -4,10 +4,19 @@ require("dotenv").config();
 const Login = require('./model/login');
 const Cidade = require('./model/cidade');
 
-const sequelize = new Sequelize('ecommerce', 'root', '', {
+const config = {
+  dialect: 'mysql',
+  dialectOptions: {
+    socketPath:  process.env.MYSQL_HOST
+  }
+}
+
+const devConfig = {
   host: process.env.MYSQL_HOST,
   dialect: 'mysql',
-});
+}
+
+const sequelize = new Sequelize('ecommerce', 'root', '', process.env.PRODUCTION ? config : devConfig);
 
 Login.init(sequelize);
 Cidade.init(sequelize);
