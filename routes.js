bb7b4ccbe889c96038c1d0b8886a6762924c7d27
ViniciusTei <viniciustprates@gmail.com>
2023-06-24 20:14:42 -0300

    feat add create new city route

--- routes.js
+++ routes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const LoginController = require("./controller/login");
 const RegisterController = require("./controller/register");
+const CidadeController = require("./controller/cidade");
+
+const auth = require("./middleware/auth");
 
 const router = express.Router();
 
 router.post('/login', LoginController.index)
 router.post('/register', RegisterController.index)
+router.post('/cidade', auth, CidadeController.index)
 
 module.exports = router;
